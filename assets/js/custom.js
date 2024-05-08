ac0e7ac4d8e162c4938d79738394cc64fc225289
devavology <145644105+devavology@users.noreply.github.com>
2024-05-08 19:01:02 +0530

    update

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -8,39 +8,37 @@ $(document).ready(function(){
 });
 
 
-$(document).ready(function(){
-    $(".drop-down-1").click(function(){
-        $(".drop-menu").addClass("show");
-    });
-    $(".cross ").click(function(){
-        $(".drop-menu").removeClass("show");
-    });
-});
-$(document).ready(function(){
-    $(".drop-down-2").click(function(){
-        $(".drop-menu2").addClass("show2");
-    });
-    $(".cross").click(function(){
-        $(".drop-menu2").removeClass("show2");
-    });
-});
+
+
+
+
+
 // $(document).ready(function(){
-//     $(".drop-down-3").click(function(){
-//         $(".drop-menu3").addClass("show3");
+//     $(".drop-down-1").click(function(){
+//         $(".drop-menu").addClass("show");
+//     });
+//     $(".cross ").click(function(){
+//         $(".drop-menu").removeClass("show");
+//     });
+// });
+
+
+// $(document).ready(function(){
+//     $(".drop-down-2").click(function(){
+//         $(".drop-menu2").addClass("show2");
 //     });
 //     $(".cross").click(function(){
-//         $(".drop-menu3").removeClass("show3");
+//         $(".drop-menu2").removeClass("show2");
 //     });
 // });
 
 
 $(document).ready(function(){
-    $('.drop-down-3').click(function(){
-        if ($('.drop-menu3').hasClass('hidden')) {
-            $('.drop-menu3').removeClass('hidden').addClass('visible').show();
-        } else {
-            $('.drop-menu3').removeClass('visible').addClass('hidden').hide();
-        }
+    $(".drop-down-3").click(function(){
+        $(".drop-menu3").addClass("show3");
+    });
+    $(" .cross").click(function(){
+        $(".drop-menu3").removeClass("show3");
     });
 });
 
